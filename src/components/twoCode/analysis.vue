72b20a4cc0e1c64111d7f5efbcc6878b3cffa8d3
FLangQi <530625456@qq.com>
2017-07-12 17:57:49 +0800

    0712
    溯源码树状图修改
    新增渠道分析页面

---
+++ src/components/twoCode/analysis.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="right-main">
+		<div class="right-main-main" id="pic">
+			
+		</div>
+	</div>
+</template>
+
+<script>
+	import common from '../../common.js';
+	export default{
+		data(){
+			return{
+
+			}
+		},
+		methods:{
+			init:function(){
+				var echarts = require('echarts');
+				var myChart = echarts.init(document.getElementById('pic'));
+				// myChart.showLoading();
+			}
+		},
+		mounted:function(){
+			this.init();
+		}
+	}
+	
+</script>
+
+<style scoped>
+	.right-main-main{
+		widows: 95%;
+		height: 500px;
+	}
+</style>
